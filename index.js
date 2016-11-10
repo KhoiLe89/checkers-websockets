ced6b2168a4c9b412a78c01a44cb8c213d248889
Khoi Le <khoileee89@gmail.com>
2016-11-10 18:19:42 -0500

    added angular

--- index.js
+++ index.js
@@ -5,12 +5,24 @@ var io = require("socket.io")(http);
 var appJs = require('./public/js/app.js')
 app.use(express.static('public'))
 
-
+// function changeColor(number, color){
+//   $("#square" + number).css("background-color", color)
+//   console.log("this function is being used")
+// }
 
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname +"/index.html")
+
 })
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    console.log('message: ' + msg);
+    var msgArray=msg.split(',')
+    // changeColor(msgArray[0],msgArray[1])
+  });
+});
+
 
 http.listen("3001", () => {
   console.log("Its working")
